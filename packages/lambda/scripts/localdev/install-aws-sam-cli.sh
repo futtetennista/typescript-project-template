69e374afdae809b551b1e265be1acffc75eee404
Stefano <323267+futtetennista@users.noreply.github.com>
2025-03-27 15:37:06 +0100

    Fix shellcheck warnings

--- packages/lambda/scripts/localdev/install-aws-sam-cli.sh
+++ packages/lambda/scripts/localdev/install-aws-sam-cli.sh
@@ -8,17 +8,17 @@ set -euo pipefail
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 if [ -f "$REPO_ROOT/.bin/aws-sam-cli/sam" ]; then
-  echo "\033[33m[debug]✅ AWS SAM CLI is already installed.\033[0m"
+  printf "\033[33m[debug]✅ AWS SAM CLI is already installed.\033[0m"
   exit 0
 fi
 
 if [ ! -f /tmp/aws-sam-cli-macos.pkg ]; then
-  echo "\033[38;5;208m[debug]🛠️ AWS SAM CLI installer download will start.\033[0m"
+  printf "\033[38;5;208m[debug]🛠️ AWS SAM CLI installer download will start.\033[0m"
   curl -L -o /tmp/aws-sam-cli-macos.pkg \
     https://github.com/aws/aws-sam-cli/releases/download/v1.131.0/aws-sam-cli-macos-x86_64.pkg
-  echo "\033[33m[debug]✅ AWS SAM CLI installer download did end.\033[0m"
+  printf "\033[33m[debug]✅ AWS SAM CLI installer download did end.\033[0m"
 else
-  echo "\033[33m[debug]✅ AWS SAM CLI already downloaded.\033[0m"
+  printf "\033[33m[debug]✅ AWS SAM CLI already downloaded.\033[0m"
 fi
 
 cat <<EOF > /tmp/install-aws-sam-cli.xml
@@ -40,7 +40,7 @@ EOF
 
 mkdir -p "$REPO_ROOT"/.bin
 
-echo "\033[38;5;208m[debug]🛠️ AWS SAM CLI installation will start.\033[0m"
+printf "\033[38;5;208m[debug]🛠️ AWS SAM CLI installation will start.\033[0m"
 
 installer -pkg /tmp/aws-sam-cli-macos.pkg \
   -target CurrentUserHomeDirectory \
@@ -48,11 +48,10 @@ installer -pkg /tmp/aws-sam-cli-macos.pkg \
 
 exit_code=$?
 
-echo "\033[33m[debug]✅ AWS SAM CLI installation did complete.\033[0m"
+printf "\033[33m[debug]✅ AWS SAM CLI installation did complete.\033[0m"
 
 if [ $exit_code -eq 0 ]; then
-  echo "\033[33m[debug]✅ AWS SAM CLI installation succeeded.\033[0m"
+  printf "\033[33m[debug]✅ AWS SAM CLI installation succeeded.\033[0m"
 else
-  echo "\033[38;5;208m[debug]🔴 AWS SAM CLI installation failed.\033[0m"
+  printf "\033[38;5;208m[debug]🔴 AWS SAM CLI installation failed.\033[0m"
 fi
-
